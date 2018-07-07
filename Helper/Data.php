b7255b16f6406b578066950111fa8a60a8295b59
Ngo Quang Cuong <bestearnmoney87@gmail.com>
2018-07-07 18:33:41 +0700

    added the tawk.to widget javascript code

--- Helper/Data.php
+++ Helper/Data.php
@@ -25,6 +25,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     const FACEBOOK_MESSENGER_ENABLE = 'phpcuong_customer_chatbox/general/facebook_messenger_enable';
     const FACEBOOK_MESSENGER_CUSTOMER_CHAT_CODE = 'phpcuong_customer_chatbox/general/facebook_messenger_customer_chat_code';
+    const TAWK_TO_WIDGET_ENABLE = 'phpcuong_customer_chatbox/general/tawk_enable';
+    const TAWK_TO_WIDGET_CODE = 'phpcuong_customer_chatbox/general/tawk_widget_code';
 
     /**
      * Retrieve the facebook messenger status
@@ -48,6 +50,32 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         return $this->scopeConfig->getValue(
             self::FACEBOOK_MESSENGER_CUSTOMER_CHAT_CODE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Retrieve the tawk.to widget status
+     *
+     * @return boolean
+     */
+    public function getTawkToWidgetStatus()
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::TAWK_TO_WIDGET_ENABLE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Retrieve the Tawk.to widget code
+     *
+     * @return string
+     */
+    public function getTawkToWidgetCode()
+    {
+        return $this->scopeConfig->getValue(
+            self::TAWK_TO_WIDGET_CODE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
     }
